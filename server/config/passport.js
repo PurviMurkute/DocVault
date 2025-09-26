3a55632145e7af7abd15649fa2c4efc7c453f355
Purvi Murkute <purvimurkute51@gmail.com>
2025-09-26 19:53:53 +0530

    backend google login code implement

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,7 +1,10 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import dotenv from 'dotenv';
+dotenv.config();
 
 passport.use(
   new LocalStrategy(
@@ -22,6 +25,36 @@ passport.use(
         return done(null, user, { message: "Login Successful" });
       } catch (error) {
         done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.SERVER_URL}/api/google/callback`,
+    },
+
+    async (accessToken, refreshToken, profile, cb) => {
+
+      try {
+        const email = profile.emails[0].value;
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            username: profile.displayName,
+            email: email
+          });
+        }
+
+        return cb(null, user, { message: "Login successful" })
+      } catch (error) {
+        return cb(error);
       }
     }
   )
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,37 @@
 import express from 'express';
-import { loginUser, registerUser } from '../controllers/user.js';
+import { getCurrentUser, loginUser, registerUser } from '../controllers/user.js';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+import verifyJwt from '../middlewares/jwt.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
+userRouter.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+userRouter.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+  }),
+  (req, res) => {
+    try {
+      const token = jwt.sign(
+        { _id: req.user._id, fullname: req.user.username, email: req.user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d" }
+      );
+      res.redirect(`${process.env.CLIENT_URL}/google-success?token=${token}`);
+    } catch (error) {
+      res.redirect(`${process.env.CLIENT_URL}/login`);
+    }
+  }
+);
+
+userRouter.get('/current-user', verifyJwt, getCurrentUser );
 
 export default userRouter;
